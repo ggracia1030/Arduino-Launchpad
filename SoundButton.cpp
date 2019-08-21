1e5728e81aaeee5128aec6789223291f30486a29
ggracia10 <guillemgracia99@gmail.com>
2019-08-21 12:20:03 +0200

    TEST

--- SoundButton.cpp
+++ SoundButton.cpp
@@ -34,7 +34,7 @@ bool SoundButton::isButtonPressed()
 {
 #if defined (__AVR__) || (__avr__)
 	//DigitalWrite(yPin, true);
-	bool value = digitalRead(xPin) == HIGH && digitalRead(yPin) == HIGH;
+	bool value = digitalRead(xPin) == HIGH;
 	if (value) {
 		String tempString = "Button Pressed | " + sound->ToString(); +" | Pin X = " + (String)xPin + " | Pin Y = " + (String)yPin;
 		Serial.println(tempString);
